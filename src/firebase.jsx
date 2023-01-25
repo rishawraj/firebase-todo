0ebe6218c2e06df803bd9055995f9c35587a2ced
rishaw <rishawraj0703@gmail.com>
2023-01-26 01:16:09 +0530

    vite config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,14 +2,6 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-// const app = firebase.initializeApp({
-//   apiKey: "AIzaSyBl7_dT4vboJRH4aaUYJBo7U_mFbglspvw",
-//   authDomain: "todo-app-84db3.firebaseapp.com",
-//   projectId: "todo-app-84db3",
-//   storageBucket: "todo-app-84db3.appspot.com",
-//   messagingSenderId: "554623523373",
-//   appId: "1:554623523373:web:9b1dcf84eaa9d1775d36a4",
-// });
 const app = firebase.initializeApp({
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
