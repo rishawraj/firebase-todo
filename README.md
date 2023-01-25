1b212189e463a1f4d6ef3538f380d40e4424827f
rishaw <rishawraj0703@gmail.com>
2023-01-26 00:59:14 +0530

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Firebase Todo App with Authentication
+
+Welcome to the Firebase Todo App with Authentication!
+
+This app allows users to create, read, update, and delete their own todo items, while also providing a secure authentication system to ensure that only authorized users can access the app.
+
+- [live app](https://rishawraj.github.io/react-chat-app/)
+
+## Features
+
+- User authentication using Firebase Authentication
+- Create, read, update, and delete todo items
+- Secure data storage using Firebase Firestore Database
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,21 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
+// const app = firebase.initializeApp({
+//   apiKey: "AIzaSyBl7_dT4vboJRH4aaUYJBo7U_mFbglspvw",
+//   authDomain: "todo-app-84db3.firebaseapp.com",
+//   projectId: "todo-app-84db3",
+//   storageBucket: "todo-app-84db3.appspot.com",
+//   messagingSenderId: "554623523373",
+//   appId: "1:554623523373:web:9b1dcf84eaa9d1775d36a4",
+// });
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyBl7_dT4vboJRH4aaUYJBo7U_mFbglspvw",
-  authDomain: "todo-app-84db3.firebaseapp.com",
-  projectId: "todo-app-84db3",
-  storageBucket: "todo-app-84db3.appspot.com",
-  messagingSenderId: "554623523373",
-  appId: "1:554623523373:web:9b1dcf84eaa9d1775d36a4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  stoargeBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 export default app;
